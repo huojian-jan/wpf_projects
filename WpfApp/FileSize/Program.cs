685528f2a67230ec3babac875a9d6ec4e52e0568
huojian-jan <3300837618@qq.com>
2024-10-28 11:22:01 +0800

    parallel test for compute file size

---
+++ WpfApp/FileSize/Program.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace FileSize
+{
+
+    internal class Program
+    {
+        static ConcurrentBag<long> OtherAccountData = new ConcurrentBag<long>();
+        //private static TimeSpan _reserveTime = DateTime.Now - DateTime.Now.AddYears(-1);
+        private static TimeSpan _reserveTime = DateTime.Now - DateTime.Now.AddYears(-1);
+        static void Main(string[] args)
+        {
+            // var candidateDirs = Directory.EnumerateDirectories(@"C:\Users\33008\AppData\Local\ShadowBot\users").Where(x => !x.Contains("Standalone") && !x.Contains("Assistant"));
+            // ComputeOtherAccountCache();
+
+            
+            
+            
+            Console.ReadKey();
+        }
+
+        private static void ParallelTest()
+        {
+            Parallel.ForEach(Enumerable.Range(0, 10), x =>
+            {
+                LongMethod();
+            });
+            
+            Console.WriteLine("Parallel Test");    
+        }
+
+
+
+        private static void LongMethod()
+        {
+            for (int i = 0; i < 100000000; i++)
+            {
+                if (i % 999 == 0)
+                {
+                    Console.Write("**************");
+                }
+            }
+        }
+
+
+
+
+
+        private static void ComputeOtherAccountCache()
+        {
+            var candidateDirs = Directory.EnumerateDirectories(@"C:\Users\33008\AppData\Local\ShadowBot\users").Where(x => !x.Contains("Standalone") && !x.Contains("Assistant"));
+            //排除调度，单机版
+            var size = 0l;
+            var tasks = new List<Task<long>>();
+            var watch = new Stopwatch();
+            watch.Start();
+            foreach (var user in candidateDirs)
+            {
+                DirSize(new DirectoryInfo(user));
+            }
+            watch.Stop();
+            foreach (var itemSize in OtherAccountData)
+            {
+                size += itemSize;
+            }
+
+            var mb = size / (1024d * 1024);
+            mb = Math.Round(mb, 3);
+            var time = watch.ElapsedMilliseconds / 1000d;
+            time = Math.Round(time, 3);
+
+            Console.WriteLine($"size={mb} MB\ttime={time}");
+
+
+        }
+
+        private static long DirSize(DirectoryInfo dirInfo, int layer = 0)
+        {
+            if (!dirInfo.Exists) return 0;
+            if (layer == 2)
+            {
+                Parallel.ForEach(dirInfo.EnumerateDirectories(), dir =>
+                {
+                    DirSize(dir, layer++);
+                });
+            }
+            else
+            {
+                foreach (var dir in dirInfo.EnumerateDirectories())
+                {
+                    DirSize(dir, layer++);
+                }
+            }
+
+            foreach (var file in dirInfo.EnumerateFiles())
+            {
+                if (DateTime.Now - file.LastWriteTime > _reserveTime)
+                {
+                    OtherAccountData.Add(file.Length);
+                }
+            }
+
+            return 0;
+        }
+
+    }
+}
--- WpfApp/My_Todo/MainWindow.xaml.cs
+++ WpfApp/My_Todo/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,43 @@ namespace My_Todo
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext=this;
+
+            Students = new ObservableCollection<Student>();
+            Students.Add(new Student("zhangsan"));
+            Students.Add(new Student("xiaowang"));
+            Students.Add(new Student("lisi"));
+
+            Test1();
+        }
+        public ObservableCollection<Student> Students { get; set; }
+
+
+
+        public void Test1()
+        {
+            try
+            {
+                var path = @"C:\Users\33008\Downloads\客户端 5.10-5.14\general";
+                var info = new FileInfo(path);
+            }
+            catch (Exception ex)
+            {
+                var a = 10;
+                var msg = ex.Message;
+            }
+
+        }
+    }
+
+
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public Student(string name)
+        {
+            Name=name;
         }
     }
 }
